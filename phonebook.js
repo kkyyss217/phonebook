f9563af93781ca6b48a14501e7833a7854507f79
kkyyss217 <kkyyss217@naver.com>
2018-04-01 23:32:09 +0900

    phonebook

--- phonebook.js
+++ phonebook.js
@@ -63,11 +63,48 @@ function updateTable(p){
   	tr.appendChild(td4);
 	table.appendChild(tr);
 }
+function tablesort(n){ //성 정렬, 이름 정렬, 번호 정렬 총 3개만들기
+    var table,  rows, switching, i, x, y,elementchange, dir, switchcount = 0;
 
-function tableSort(m,n){
-	return m-n;
-}
+    table  = document.getElementById("mytable");
+    switching = true;
+   
+     dir = "일회용"; 
+   
+      while (switching) {
+      switching = false; 
+      rows =  table.getElementsByTagName("tr"); //rows를 유사배열로만듬
+     
+        for (i = 1; i < (rows.length - 1); i++) {  
+          elementchange = false;
+          x = rows[i].getElementsByTagName("td")[n];
+          y = rows[i + 1].getElementsByTagName("td")[n];
 
-function tableSort(m,n){
-	return n-m;
+            if (dir  == "일회용") {//크기비교 x가 클때 
+              if (x.innerHTML.toLowerCase()  > y.innerHTML.toLowerCase()) { //tolowercase : 소문자로변경
+                elementchange= true;
+                break; //for종료
+
+                    }
+                      } 
+
+                else if (dir == "비교") {//크기비교 y가 클때
+                      if (x.innerHTML.toLowerCase()  < y.innerHTML.toLowerCase()) { //tolowercase : 소문자로변경 
+                            elementchange= true; //elementchange = 엘리먼트 순서변경
+                            break; //for종료
+               }
+             }
+           }   
+       if (elementchange) {//node순서변경
+          rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);//rows[i+1]이라는 엘리먼트를 rows[i]위에 넣어라
+          switching = true; 
+          switchcount  ++; 
+          } 
+        else {
+          if (switchcount == 0) {
+                  dir = "비교";
+                  switching = true;
+          }
+       }
+     }
 }
